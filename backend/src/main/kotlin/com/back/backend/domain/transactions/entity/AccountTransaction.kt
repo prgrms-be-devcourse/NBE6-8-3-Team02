6ab57e1753328d16e91c078994a764db900154a9
Whitedoggy <nubuli19@gmail.com>
2025-08-27 14:47:23 +0900

    거래 도메인 Entity 추가.

--- backend/src/main/kotlin/com/back/backend/domain/transactions/entity/AccountTransaction.kt
+++ backend/src/main/kotlin/com/back/backend/domain/transactions/entity/AccountTransaction.kt
@@ -1,4 +1,27 @@
 package com.back.backend.domain.transactions.entity
 
-class AccountTransaction {
+import com.back.backend.global.jpa.entity.BaseEntity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import java.time.LocalDateTime
+
+class AccountTransaction : BaseEntity() {
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "account_id", nullable = false)
+//    @OnDelete(action = OnDeleteAction.CASCADE)
+//    var account : Account? = null;
+    // Account 도메인이 추가되었을 시에 추가.
+
+    @Enumerated(EnumType.STRING)
+    var type: TransactionType = TransactionType.REMOVE
+
+    var amount: Long? = 0L
+
+    var content: String? = null // 필요 메모
+
+    var date : LocalDateTime? = null // 체결일
+
+    // int id(PK) -> BaseEntity
+    // LocalDateTime created_at -> BaseEntity
+    // LocalDateTime modified_at -> BaseEntity
 }
