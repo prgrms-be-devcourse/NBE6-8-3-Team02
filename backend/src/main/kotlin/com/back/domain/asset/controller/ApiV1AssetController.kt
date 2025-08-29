5132b9dd5af035bcd08aa09a5368d943276f1e71
jaegyun1011 <jaegyun1011@naver.com>
2025-08-29 15:03:01 +0900

    feat(be): AssetController 구현, AssetService 예외처리

--- backend/src/main/kotlin/com/back/domain/asset/controller/ApiV1AssetController.kt
+++ backend/src/main/kotlin/com/back/domain/asset/controller/ApiV1AssetController.kt
@@ -2,16 +2,16 @@ package com.back.domain.asset.controller
 
 import com.back.domain.asset.dto.AssetDto
 import com.back.domain.asset.dto.CreateAssetRequestDto
-import com.back.domain.asset.entity.Asset
+import com.back.domain.asset.dto.CreateWithoutMemberDto
+import com.back.domain.asset.dto.UpdateAssetRequestDto
 import com.back.domain.asset.service.AssetService
+import com.back.global.rsData.RsData
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.*
 
 
 @RestController
@@ -21,5 +21,92 @@ class ApiV1AssetController(
     private val assetService: AssetService
 ) {
     // 생성
+    @PostMapping
+    @Operation(summary = "자산 등록")
+    fun createAsset(
+        @RequestBody createAssetRequestDto: CreateAssetRequestDto
+    ): ResponseEntity<RsData<AssetDto>> {
+        val asset = assetService.createAsset(createAssetRequestDto)
+        val assetDto = AssetDto(asset)
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(RsData("200-1", "자산이 등록되었습니다.", assetDto))
+    }
 
+    // 다건 조회
+    @GetMapping
+    @Operation(summary = "자산 다건 조회")
+    fun getAssets(): ResponseEntity<RsData<List<AssetDto>>> {
+        val assets = assetService.findAll()
+        val assetDtos = assets.map { asset -> AssetDto(asset) }
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(RsData("200-1", "자산 목록을 조회했습니다.", assetDtos))
+    }
+
+    // 단건 조회
+    @GetMapping("/{id}")
+    @Operation(summary = "자산 단건 조회")
+    fun getAsset(
+        @PathVariable id: Int
+    ): ResponseEntity<RsData<AssetDto>> {
+        val asset = assetService.findById(id)
+        val assetDto = AssetDto(asset)
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(RsData("200-1", "${id}번 자산을 조회했습니다.", assetDto))
+    }
+
+    // id 기반 삭제
+    @DeleteMapping("/{id}")
+    @Operation(summary = "자산 삭제 (id 기반)")
+    fun deleteAsset(
+        @PathVariable id: Int
+    ): ResponseEntity<RsData<AssetDto>> {
+        val asset = assetService.deleteById(id)
+        val assetDto = AssetDto(asset)
+        return ResponseEntity
+            .status(HttpStatus.NO_CONTENT)
+            .body(RsData("200-1", "${id}번 자산을 삭제했습니다.", assetDto))
+    }
+
+    // id 기반 수정
+    @PutMapping("/{id}")
+    @Operation(summary = "자산 수정 (id 기반)")
+    fun updateAsset(
+        @RequestBody updateAssetRequestDto: UpdateAssetRequestDto
+    ): ResponseEntity<RsData<AssetDto>> {
+        val asset = assetService.updateById(updateAssetRequestDto)
+        val assetDto = AssetDto(asset)
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(RsData("200-1", "${updateAssetRequestDto.id}번 자산을 수정했습니다.", assetDto))
+    }
+
+    @GetMapping("/member")
+    @Operation(summary = "사용자 기반 자산 목록 조회")
+    fun getAssetsByCurrentMember(
+        @AuthenticationPrincipal userDetails: CustomUserDetails
+    ): ResponseEntity<RsData<List<AssetDto>>> {
+        val memberId = userDetails.member.id
+        val assets = assetService.findAllByMemberId(memberId)
+        val assetDtos = assets.map { asset -> AssetDto(asset) }
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(RsData("200-1", "${memberId}번 사용자의 자산 목록을 조회했습니다.", assetDtos))
+    }
+
+    @PostMapping("/member")
+    @Operation(summary = "사용자 기반 자산 등록")
+    fun createAssetByCurrentMember(
+        @AuthenticationPrincipal userDetails: CustomUserDetails,
+        @RequestBody createWithoutMemberDto: CreateWithoutMemberDto
+    ): ResponseEntity<RsData<AssetDto>> {
+        val memberId = userDetails.member.id
+        val asset = assetService.createAssetByMember(memberId, createWithoutMemberDto)
+        val assetDto = AssetDto(asset)
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(RsData("200-1", "자산이 등록되었습니다.", assetDto))
+    }
 }
